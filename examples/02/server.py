28d84a1cbb80c07cbde65ca6d45c0d0bebdbc509
Leo Vidarte <lvidarte@gmail.com>
2016-10-13 15:12:12 -0300

    Add author and license on all files

--- examples/02/server.py
+++ examples/02/server.py
@@ -1,3 +1,13 @@
+"""
+Author: Leo Vidarte <http://nerdlabs.com.ar>
+
+This is free software,
+you can redistribute it and/or modify it
+under the terms of the GPL version 3
+as published by the Free Software Foundation.
+
+"""
+
 import sqlite3
 import os.path
 from flask import Flask, request, g, redirect, url_for, \
